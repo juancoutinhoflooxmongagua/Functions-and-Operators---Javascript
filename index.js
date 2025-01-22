caeed04a5ec261599bb798556d6796917c053fe2
Juan Coutinho <juan.devx@gmail.com>
2025-01-22 12:31:18 -0300

    (feat) First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// Console Log no Javascript
+
+var OlaMundo = "Olá Mundo"
+
+console.log(OlaMundo)
+
+console.log('OlaMundo')
+
+// Declaração de Variaveis no Javascript
+
+let a = 10;
+
+const b = 10;
+
+console.log(a === b)
+
+console.log(a != b)
+
+// && End and Or 
+
+console.log(a === b || typeof a == "string")
+
+// If e Else
+
+let cor = "yellow"
+
+if (cor === "Green"){
+    console.log("Go");
+} 
+
+else if(cor === "yellow") {
+    console.log("Take Care");
+}
+
+else {  
+    console.log("Stop")
+}
